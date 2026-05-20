8efb5bb15ed44eb0ad60e42edb29d424373dca9b
Кабаков Иван <i.kabakov@sibstrin.ru>
2026-05-20 15:33:07 +0700

    Создал структуру infrastructure

---
+++ JointTripService/Infrastructure/JointTripService.Infrastructure.EntityFramework/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using JointTripService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using JointTripService.Domain;
+
+namespace NotesService.Infrastructure.EntityFramework;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Note> Notes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+
+
+}
---
+++ JointTripService/Infrastructure/JointTripService.Infrastructure.EntityFramework/Configurations/BookingConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JointTripService.Infrastructure.EntityFramework.Configurations
+{
+    internal class BookingConfiguration
+    {
+    }
+}
